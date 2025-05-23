60bd7a102ad6ea53a27251e3bbea74832a91408f
Nahuel Leyes <nleyes02@gmail.com>
2025-05-23 20:18:53 -0300

    cambios generales2

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ProductoList from './components/ProductoList';
 import ProductoDetail from './components/ProductoDetail';
-import Home from './components/Home'; // Importa el componente Home
+import Home from './components/Home'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Mapa from './components/Mapa';
 import CrearProductoForm from './components/CrearProductoForm';
@@ -15,41 +15,41 @@ import ChartsGoogle from './components/ChartsGoogle';
 import Signup from './components/SignUp';
 import InventarioList from './components/InventarioList';
 import Perfil from './components/Perfil';
+import Perros from './components/Perros'; // Sección de Perros
+import Menu from './components/Menu'; // Agregamos el menú
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './components/Footer';
 import { CarritoProvider } from './components/CarritoContext';
-
 
 const App: React.FC = () => {
   return (
     <AuthProvider>
       <CarritoProvider>
-          <Router>
-        <div>
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/mapa" element={<Mapa />} />
-                <Route path="/productos" element={<ProductoList />} />
-                <Route path="/producto/:id" element={<ProductoDetail />} />
-                <Route path="/perfil" element={<RutaPrivada><Perfil /></RutaPrivada>} />
-                <Route path="/inventario" element={<RutaPrivada><InventarioList /></RutaPrivada>} />
-                <Route path="/crear-producto" element={<RutaPrivada><CrearProductoForm /></RutaPrivada>} />
-                <Route path="/productos/:id/modificar" element={<RutaPrivada><ModificarProducto /></RutaPrivada>} />
-                <Route path="/mercadopago" element={<RutaPrivada><CheckoutMP /></RutaPrivada>} />
-                <Route path="/google-charts" element={<RutaPrivada><ChartsGoogle /></RutaPrivada>} />
-                <Route path="*" element={<Home />} />
-              </Routes>
-            </div>
-            <Footer />
-          </Router>
+        <Router>
+          <Menu /> {/* Colocamos el menú aquí para que siempre esté visible */}
+          <div>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/mapa" element={<Mapa />} />
+              <Route path="/productos" element={<ProductoList />} />
+              <Route path="/producto/:id" element={<ProductoDetail />} />
+              <Route path="/perfil" element={<RutaPrivada><Perfil /></RutaPrivada>} />
+              <Route path="/inventario" element={<RutaPrivada><InventarioList /></RutaPrivada>} />
+              <Route path="/crear-producto" element={<RutaPrivada><CrearProductoForm /></RutaPrivada>} />
+              <Route path="/productos/:id/modificar" element={<RutaPrivada><ModificarProducto /></RutaPrivada>} />
+              <Route path="/mercadopago" element={<RutaPrivada><CheckoutMP /></RutaPrivada>} />
+              <Route path="/google-charts" element={<RutaPrivada><ChartsGoogle /></RutaPrivada>} />
+              <Route path="/perros" element={<Perros />} /> {/* Nueva ruta para la sección Perros */}
+              <Route path="*" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </Router>
       </CarritoProvider>
     </AuthProvider>
   );
 };
-<div className="full-width-section" style={{ backgroundColor: '#8e2673' }}>
-  {/* contenido */}
-</div>
 
 export default App;
